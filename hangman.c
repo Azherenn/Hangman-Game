579e5ef9fcd925c5a393a64eae04e8f236dfda0e
Magno <magno.ferreira2408@gmail.com>
2025-12-11 20:15:10 -0300

    Initial project structure

---
+++ hangman.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // Aqui vai ficar a lógica do jogo
+    printf("Bem-vindo ao Jogo da Forca!\n");
+    printf("O ambiente de desenvolvimento está configurado.\n");
+
+    return 0;
+}
