b0e503b7fe2c06a9ca21f758847cb43566b8a193
Magno <magno.ferreira2408@gmail.com>
2025-12-14 13:04:27 -0300

    Introduction of pointers with implementation for organizing .txt files.

--- hangman.c
+++ hangman.c
@@ -3,9 +3,22 @@
 #include <string.h>
 
 int main() {
-    // Aqui vai ficar a lógica do jogo
-    printf("Bem-vindo ao Jogo da Forca!\n");
-    printf("O ambiente de desenvolvimento está configurado.\n");
+
+    FILE *database;
+    database = fopen("database.txt", "r");
+
+    if(database == NULL){
+        printf("Arquivo não existe\n");
+        getchar();
+        exit(0);
+    }
+
+    char words[100];
+
+    while(fgets (words, 100, database) !=NULL){
+        printf("%s\n", words);
+    }
+    fclose(database);
 
     return 0;
 }
